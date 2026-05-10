e5e5bc33b8305ada27fe452be5ebc0763be1f1f7
JenniferSiawiti <jennifersi4witi@gmail.com>
2026-05-10 09:38:33 +0700

    Update addReportScript.js

--- addReportScript.js
+++ addReportScript.js
@@ -47,41 +47,36 @@ let uploadedImageData = "";
 
 const classesByFloor = {
   "19th": [
-    "01", "02", "03", "04", "05",
-    "06", "07", "08", "09", "10",
-    "11", "12", "13", "14", "15",
+    "01", "02", "03", "04", "15",
     "16", "17", "18", "19", "20",
     "SL", "Clinic", "Counseling",
-    "Lecturer Room", "Tutoring Room",
-    "Toilet ♀", "Toilet ♂", "Other"
+    "Lecturer Office", "Musholla", "Tutoring Room",
+    "BEM Room", "Toilet ♀", "Toilet ♂", "Other"
   ],
   "7th": [
-    "01", "02", "03", "04", "05",
-    "06", "07", "08", "09", "10",
-    "11", "12", "13", "14", "15",
-    "16", "17", "18", "19", "20",
-    "SL", "Toilet ♀", "Toilet ♂",
-    "Other", "CC", "TC", "CL",
-    "IoTL", "LA", "CR", "UA"
+    "02", "03", "04", "Dean Office",
+    "Prayer Room", "Lecturer Office", "GA Storage",
+    "Green Screen", "CC I", "CC II", "Gen Science Lab",
+    "Cyber Physical Systems Lab", "Head Of Units Office",
+    "Toilet ♀", "Toilet ♂", "Other"
   ],
   "6th": [
      "01", "02", "03",
     "Library", "Office",
-    "Toilet ♀", "Toilet ♂",
     "Meeting Room Soekarno",
     "Meeting Room Sjahrir",
     "Meeting Room Hatta",
     "Library Staff Office",
     "Student Affairs Office",
+    "Toilet ♀", "Toilet ♂",
     "Other"
   ],
   "LG": [
-    "CS Lab", "IS Lab", "MEI", "CL",
-    "WL", "Toilet ♀", "Toilet ♂",
-    "CS&IS Lab", "IE Lab",
+    "CS Lab", "IS Lab", "MEI", "Computer Lab",
+    "Wet Lab", "Toilet ♀", "Toilet ♂",
+    "CS & IS Lab", "IE Lab",
     "Lab LG", "Mechatronic Lab",
-    "Facilities Office",
-    "Wet Lab", "Other"
+    "Facilities Office", "Other"
   ]
 };
 
@@ -217,8 +212,24 @@ facilityButtons.addEventListener("click", event => {
     return;
     }
 
-  selectedFacility = btn.textContent.trim();
+selectedFacility = btn.textContent.trim();
+
+if (selectedFacility === "Toilet") {
+
+  const isFemale = selectedClass.includes("♀");
+
+  const iconFile = isFemale
+    ? "Girl-Icon.png"
+    : "Boy-Icon.png";
+
+  facilityText.innerHTML = `
+    Toilet
+    <img src="${iconFile}" class="toilet-icon">
+  `;
+
+} else {
   facilityText.textContent = selectedFacility;
+}
 
   [...facilityButtons.children].forEach(button => {
     button.classList.add("hidden");
